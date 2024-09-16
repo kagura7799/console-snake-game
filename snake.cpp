32e4121ca01ad8e48864782fbe3773675af853b0
kagura7799 <mkotelnikov97@gmail.com>
2024-09-16 20:09:18 +0300

    snake movement arrows

--- snake.cpp
+++ snake.cpp
@@ -1,61 +1,117 @@
-#include <iostream>
+#include <ncurses.h>
 #include <unistd.h>
+#include <iostream>
 
 const int WIDTH = 30;
 const int HEIGHT = 13;
-const int MAX_LEN_SNAKE = (WIDTH - 3) * (HEIGHT - 2);
-
-const int UP = 0;
-const int DOWN = 1;
-const int LEFT = 2;
-const int RIGHT = 3;
-
-int snake_dir = UP;
+const int MAX_LEN_SNAKE = (WIDTH - 2) * (HEIGHT - 2);
 
 bool gameRunning = true;
 
-char map[] =
-"##############################\n"
-"#                            #\n"
-"#                            #\n"
-"#                            #\n"
-"#                            #\n"
-"#                            #\n"
-"#                            #\n"
-"#                            #\n"
-"#                            #\n"
-"#                            #\n"
-"#                            #\n"
-"#                            #\n"
-"##############################\n";
+char map[HEIGHT][WIDTH + 1] = {
+    "##############################",
+    "#                            #",
+    "#                            #",
+    "#                            #",
+    "#                            #",
+    "#                            #",
+    "#                            #",
+    "#                            #",
+    "#                            #",
+    "#                            #",
+    "#                            #",
+    "##############################"
+};
 
 char snake = 'O';
-
 int snake_x[MAX_LEN_SNAKE] = {0};
 int snake_y[MAX_LEN_SNAKE] = {0};
 int snake_len = 1;
 
-void cleanScreen()
+void drawMap() 
 {
-    std::cout << "\033[2J\033[H";
+    clear();  
+
+    for (int y = 0; y < HEIGHT; ++y) 
+    {
+        for (int x = 0; x < WIDTH; ++x) 
+        {
+            mvaddch(y, x, map[y][x]);
+        }
+    }
+    refresh();
 }
 
-int main()
+void updateSnake() 
 {
+    for (int i = snake_len - 1; i > 0; --i) 
+    {
+        snake_x[i] = snake_x[i - 1];
+        snake_y[i] = snake_y[i - 1];
+    }
+}
+
+void placeSnake() {
+    for (int i = 0; i < snake_len; ++i) 
+    {
+        map[snake_y[i]][snake_x[i]] = snake;
+    }
+}
+
+void clearSnake() 
+{
+    for (int i = 0; i < snake_len; ++i) 
+    {
+        map[snake_y[i]][snake_x[i]] = ' ';
+    }
+}
+
+int main() {
+    initscr();  
+    cbreak();
+    keypad(stdscr, TRUE);
+    noecho();
+
     snake_x[0] = WIDTH / 2;
     snake_y[0] = HEIGHT / 2;
 
-    while (gameRunning)
+    while (gameRunning) 
     {
-        cleanScreen();
+        usleep(2000);
 
-        for (int i = 0; i < snake_len; ++i)
+        int ch = getch();
+
+        clearSnake();
+        updateSnake();
+
+        switch (ch) 
         {
-            map[snake_y[i] * WIDTH + snake_x[i]] = snake;
+            case KEY_UP:
+                --snake_y[0];
+                break;
+            case KEY_DOWN: 
+                ++snake_y[0]; 
+                break;
+            case KEY_RIGHT: 
+                ++snake_x[0]; 
+                break;
+            case KEY_LEFT: 
+                --snake_x[0]; 
+                break;
+            case 'q':
+                gameRunning = false;
+                break;
         }
-        std::cout << map << std::endl;
-        usleep(200000);
+
+        if (snake_x[0] < 1 || snake_x[0] >= WIDTH - 1 || snake_y[0] < 1 || snake_y[0] == HEIGHT - 2)
+        {
+            gameRunning = false;
+        }
+
+        placeSnake();
+        drawMap();
     }
 
+    endwin();
     return 0;
 }
